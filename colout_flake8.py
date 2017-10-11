cc0ccc66e4fdcc4bae0f7854825dde6a021e1e87
Christophe Simonis <christophe@kn.gl>
2017-10-11 14:02:57 +0200

    [IMP] adapt colout theme

--- colout_flake8.py
+++ colout_flake8.py
@@ -1,13 +1,15 @@
 def theme():
     levels = [
-        ('E\d{3}', 'red'),
-        ('W\d{3}', 'yellow'),
-        ('F\d{3}', 'magenta'),  # PyFlake errors
-        ('C9\d{2}', 'cyan'),    # McCabe complexity
-        ('N8\d{2}', 'blue'),    # Naming
+        ('E\d{3}', 'red', ''),
+        ('W\d{3}', 'yellow', ''),
+        ('F\d{3}', 'magenta', ''),      # PyFlake errors
+        ('C9\d{2}', 'cyan', ''),        # McCabe complexity
+        ('N8\d{2}', 'blue', ''),        # Naming
+        ('B\d{3}', 'yellow', 'reverse'),    # bugbear
+        ('Q4\d{2}', 'blue', 'reverse'),     # flake8-SQL
     ]
 
     return [
-        [".+:\d+:\d+: (%s) .+" % r, c]
-        for r, c in levels
+        [".+:\d+:\d+: (%s) .+" % r, c, s]
+        for r, c, s in levels
     ]
